2838e0c0b66ae13ff3ec2b9e0aad16ef4479e2ef
nikita.pankratov <nikita@pankratov.us>
2017-12-04 19:07:40 -0800

    1. add sequelize 2. add model User in sequelize 3. add AuthController for serverside validation 4. add AuthControllerPolicy using Joi for managing incoming data 5. add Vuetify for styling of frontend

--- server/src/app.js
+++ server/src/app.js
@@ -3,15 +3,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const { sequelize } = require('./models')
+const config = require('./config/config')
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email} your user was registered! hurrah!`
-  })
-})
+require('./routes')(app)
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync({force: false})
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
+  })
---
+++ server/src/routes.js
@@ -0,0 +1,10 @@
+const AuthController = require('./controllers/AuthController')
+
+const AuthControllerPolicy = require('./policies/AuthControllerPolicy')
+
+module.exports = (app) => {
+  app.post('/register',
+    AuthControllerPolicy.register,
+    AuthController.register
+  )
+}
